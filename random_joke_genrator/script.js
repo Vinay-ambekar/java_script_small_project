16b4295094b56f2fd9db834359bf198c82285a26
Vinay-ambekar <vinayambekar.b@gmail.com>
2023-04-25 08:54:03 +0530

    commited

---
+++ random_joke_genrator/script.js
@@ -0,0 +1,111 @@
+let myjokes = [
+    {
+    
+      "joke": "// This line doesn't actually do anything, but the code stops working when I delete it.",
+      
+    },
+    {
+     
+      "joke": "\"Honey, go to the store and buy some eggs.\"\n\"OK.\"\n\"Oh and while you're there, get some milk.\"\nHe never returned.",
+    
+    },
+    {
+      
+      "joke": "Saying that Java is nice because it works on every OS is like saying that anal sex is nice because it works on every gender.",
+    
+    },
+    {
+    
+      "joke": "Hey Girl,\nRoses are #ff0000,\nViolets are #0000ff,\nI use hex codes,\nBut I'd use RGB for you.",
+
+    },
+    {
+      
+      "joke": "\"We messed up the keming again guys.\"",
+     
+    },
+    {
+      
+      "joke": "A SQL statement walks into a bar and sees two tables.\nIt approaches, and asks \"may I join you?\"",
+   
+    },
+    {
+    
+      "joke": "Two SQL tables sit at the bar. A query approaches and asks \"Can I join you?\"",
+   
+    },
+    {
+   
+      "joke": "Documentation is like sex:\nWhen it's good, it's very good.\nWhen it's bad, it's better than nothing...",
+    
+    },
+    {
+   
+      "joke": "\"Can I tell you a TCP joke?\"\n\"Please tell me a TCP joke.\"\n\"OK, I'll tell you a TCP joke.\"",
+    
+    },
+    {
+     
+      "joke": "A byte walks into a bar looking miserable.\nThe bartender asks it: \"What's wrong buddy?\"\n\"Parity error.\" it replies. \n\"Ah that makes sense, I thought you looked a bit off.\"",
+  
+    },
+    {
+      "joke": "UDP is better in the COVID era since it avoids unnecessary handshakes.",
+  
+    },
+    {
+      
+      "joke": "I'll never forget my Granddad's last words to me just before he died. \"Are you still holding the ladder?\"",
+  
+    },
+    {
+      
+      "joke": "My wife is really mad at the fact that I have no sense of direction.\nSo I packed up my stuff and right.",
+
+    },
+    {
+    
+      "joke": "Debugging is like being the detective in a crime movie where you're also the murderer at the same time.",
+     
+    },
+    {
+      
+      "joke": "The glass is neither half-full nor half-empty, the glass is twice as big as it needs to be.",
+   
+    },
+    {
+      
+      "joke": "Eight bytes walk into a bar.\nThe bartender asks, \"Can I get you anything?\"\n\"Yeah,\" reply the bytes.\n\"Make us a double.\"",
+      
+    },
+    {
+     
+      "joke": "How do you make holy water? You boil the hell out of it.",
+     
+    },
+    {
+     
+      "joke": "I've got a really good UDP joke to tell you but I don’t know if you'll get it.",
+ 
+    },
+    {
+      "joke": "I hate double standards. Burn a body at a crematorium, you're \"being a respectful friend.\" Do it at home and you're \"destroying evidence.\"",
+    
+    },
+    {
+     
+        
+      "joke": "What does the MacBook have in common with Donald Trump?\n\nI would tell you....\nBut I don't compare apples to oranges.",
+   
+    }
+  
+  
+  
+  ]
+  
+  let index = Math.floor(Math.random() * (myjokes.length -1)) 
+  console.log(index)                                          
+  console.log(myjokes.length)                                
+  joke.innerHTML = myjokes[index].joke
+  joke1.innerHTML = myjokes[18].joke
+  
