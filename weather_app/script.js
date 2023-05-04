992fbd330d065a4d4ffcf06dc3319a670cd09805
Vinay-ambekar <vinayambekar.b@gmail.com>
2023-05-04 12:26:37 +0530

    commited

--- weather_app/script.js
+++ weather_app/script.js
@@ -1,1 +1,10 @@
 const apikey="47da9c1154msh460450777b706cfp12bff4jsne6f84fba72ff"
+const weatherDataE1=document.getElementById("weather-data")
+const cityInputE1=document.getElementById("city-input")
+
+const formE1=document.querySelector("form")
+formE1.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    const cityvalue=cityInputE1.value;
+    console.log(cityvalue)
+})
